54f17ba049bea761ceb0ce7e4811d5a14c989cf8
Nate Kidwell <nate@ludicast.com>
2011-06-27 14:05:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrozenBirds::Application.config.secret_token = '2f610286d4fb23e50ee021f59b540eb6f8bec245499e22996c03c87fb4175edb4ad06f80e85e2bd25e0dd7281d3244d4756b1391efd01af74e1e83cbf52aef63'
